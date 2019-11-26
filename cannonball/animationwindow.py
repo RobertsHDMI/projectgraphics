756cc1e38559a9e88452f94b3ee60e7ee9442700
Project Graphic <projectgraphics@graphic.com>
2019-11-26 23:16:27 +0100

    fixed imports and added finishing code to make it work

--- cannonball/animationwindow.py
+++ cannonball/animationwindow.py
@@ -1,4 +1,6 @@
 from graphics import *
+from cannonball.custominput import InputDialog
+from cannonball.shottracker import ShotTracker
 
 
 def main():
@@ -13,3 +15,25 @@ def main():
     for x in range(0, 210, 50):
         Text(Point(x, -5), str(x)).draw(win)
         Line(Point(x, 0), Point(x, 2)).draw(win)
+
+    # event loop, each time through fires a single shot
+    angle, vel, height = 45.0, 40.0, 2.0
+    while True:
+        # interact with the user
+        inputwin = InputDialog(angle, vel, height)
+        choice = inputwin.interact()
+        inputwin.close()
+
+        if choice == "Quit":  # loop exit
+            break
+
+        # create a shot and track until it hits ground or leaves window
+        angle, vel, height = inputwin.getValues()
+        shot = ShotTracker(win, angle, vel, height)
+        while 0 <= shot.getY() and -10 < shot.getX() <= 210:
+            shot.update(1 / 50)
+            update(50)
+
+    win.close()
+
+main()
