8cb317817072d617868f523c0e3fd3bd4b9b3566
Project Graphic <projectgraphics@graphic.com>
2019-11-26 22:20:49 +0100

    fixed indent

--- die_cast/button.py
+++ die_cast/button.py
@@ -10,22 +10,22 @@ class Button:
     """
 
 
-def __init__(self, win, center, width, height, label):
-    """Creates a rectangular button , eg:
-    qb = Button (myWin , centerPoint , width , height , 'Quit')"""
+    def __init__(self, win, center, width, height, label):
+        """Creates a rectangular button , eg:
+        qb = Button (myWin , centerPoint , width , height , 'Quit')"""
 
-    w, h = width / 2.0, height / 2.0
-    x, y = center.getX(), center.getY()
-    self.xmax, self.xmin = x + w, x - w
-    self.ymax, self.ymin = y + h, y - h
-    p1 = Point(self.xmin, self.ymin)
-    p2 = Point(self.xmax, self.ymax)
-    self.rect = Rectangle(p1, p2)
-    self.rect.setFill('lightgray')
-    self.rect.draw(win)
-    self.label = Text(center, label)
-    self.label.draw(win)
-    self.deactivate()
+        w, h = width / 2.0, height / 2.0
+        x, y = center.getX(), center.getY()
+        self.xmax, self.xmin = x + w, x - w
+        self.ymax, self.ymin = y + h, y - h
+        p1 = Point(self.xmin, self.ymin)
+        p2 = Point(self.xmax, self.ymax)
+        self.rect = Rectangle(p1, p2)
+        self.rect.setFill('lightgray')
+        self.rect.draw(win)
+        self.label = Text(center, label)
+        self.label.draw(win)
+        self.deactivate()
 
     def clicked(self, p):
         """Returns true if button active and p is inside"""
@@ -42,3 +42,5 @@ def __init__(self, win, center, width, height, label):
         self.label.setFill('black')
         self.rect.setWidth(2)
         self.active = True
+
+
