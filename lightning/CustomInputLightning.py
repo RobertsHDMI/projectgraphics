0070cea6448f9350a13b69caeb8cb23055d9f4b2
Project Graphic <projectgraphics@graphic.com>
2019-12-25 16:53:54 +0100

    added class for buttons for lighthninf file

---
+++ lightning/CustomInputLightning.py
@@ -0,0 +1,30 @@
+from graphics import *
+from lightning.ButtonLightning import Button
+
+class InputDialog:
+    """A custom window for getting simulation values (angle, velocity,
+    and height) from the user."""
+
+    def __init__(self, angle, vel, height):
+        """Build and display the input window"""
+        self.win = win = GraphWin("Initial Values", 200, 300)
+        win.setCoords(0, 0, 10, 10)
+
+        self.summer = Button(win, Point(2, 2), 4, 2, "Summer")
+        self.summer.activate()
+        self.winter = Button(win, Point(2, 4), 4, 2, "Winter")
+        self.winter.activate()
+
+    def interact(self):
+        """ wait for user to click Quit or Fire button
+        Returns a string indicating which button was clicked"""
+        while True:
+            pt = self.win.getMouse()
+            if self.summer.clicked(pt):
+                return "Summer"
+            if self.winter.clicked(pt):
+                return "Winter"
+
+    def close(self):
+        """close the input window"""
+        self.win.close()
